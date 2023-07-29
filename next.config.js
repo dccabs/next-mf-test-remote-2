77537b6230c30592c0d139c4f48ec50eb9215552
Dan Cabaniss <dccabs@gmail.com>
2023-07-29 20:15:22 +0100

    remove uneccessary config

--- next.config.js
+++ next.config.js
@@ -1,24 +1,5 @@
 /** @type {import('next').NextConfig} */
 const NextFederationPlugin = require('@module-federation/nextjs-mf');
-const packageJson = require('./package.json');
-const sharedCongfigDev = {}
-const sharedCongfigProd = {
-    // ...packageJson.dependencies,
-    // react: {
-    //     singleton: true,
-    //     requiredVersion: packageJson.dependencies.react
-    // },
-    // "react-dom": {
-    //     singleton: true,
-    //     requiredVersion: packageJson.dependencies["react-dom"]
-    // },
-    // "react/jsx-dev-runtime": {
-    //     singleton: true,
-    //     requiredVersion: packageJson.dependencies["react"]
-    // },
-}
-
-const sharedConfig = process.env.NODE_ENV === 'production' ? sharedCongfigProd : sharedCongfigDev;
 
 const nextConfig = {
   reactStrictMode: true,
@@ -36,7 +17,6 @@ const nextConfig = {
                     './secondNav': './components/SecondNav.js',
                     './header': './components/Header.js',
                 },
-                shared: sharedCongfigProd,
             })
         );
         config.resolve.fallback = { fs: false, module: false }
